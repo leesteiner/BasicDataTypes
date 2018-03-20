240f5a12f0c4392e68249e50a9f456a0afa5a971
Lee Steiner <leealansteiner@gmail.com>
2018-03-20 18:09:40 -0400

    Added 4 methods regarding functionality and parsing.

--- BasicDataTypes/Program.cs
+++ BasicDataTypes/Program.cs
@@ -13,6 +13,10 @@ namespace BasicDataTypes
             LocalVarDeclaration();
             DefaultDeclarations();
             NewingDataTypes();
+            ObjectFunctionality();
+            CharFunctionality();
+            ParseFromStrings();
+            ParseFromStringWithTryParse();
         }
 
         static void LocalVarDeclaration()
@@ -52,6 +56,63 @@ namespace BasicDataTypes
             double d = new double();        //set to 0;
             DateTime dt = new DateTime();   //set to 1/1/0001 12:00:00AM
             Console.WriteLine("{0},{1},{2},{3}", b, i, d, dt);
+            Console.WriteLine();
+        }
+
+        static void ObjectFunctionality()
+        {
+            Console.WriteLine("=> System.Object Functionality:");
+            //A C# int is really a shorthand for System. Int32,
+            //which inherits the following members from System.Object:
+            Console.WriteLine("12.GetHashCode() = {0}", 12.GetHashCode());
+            Console.WriteLine("12.Equals(23) = {0}", 12.Equals(23));
+            Console.WriteLine("12.ToString() = {0}", 12.ToString());
+            Console.WriteLine("12.GetType() = {0}", 12.GetType());
+            Console.WriteLine();
+        }
+
+        static void CharFunctionality()
+        {
+            Console.WriteLine("=> char type functionality:");
+            char myChar = 'a';
+            Console.WriteLine("char.IsDigit('a'); {0}", char.IsDigit(myChar));
+            Console.WriteLine("char.IsLetter('a'); {0}", char.IsLetter(myChar));
+            Console.WriteLine("char.IsWhiteSpace('Hello There', 5): {0}", char.IsWhiteSpace("Hello there", 5));
+            Console.WriteLine("char.IsWhiteSpace('Hello there', 6): {0}", char.IsWhiteSpace("Hello there", 6));
+            Console.WriteLine("char.IsPunctuation('?'): {0}", char.IsPunctuation('?'));
+            Console.WriteLine();
+        }
+
+        static void ParseFromStrings()
+        {
+            Console.WriteLine("=> Data type parsing:");
+            bool b = bool.Parse("True");
+            Console.WriteLine("Value of b: {0}", b);
+            double d = double.Parse("99.884");
+            Console.WriteLine("Value of d: {0}", d);
+            int i = int.Parse("8");
+            Console.WriteLine("Value of i: {0}", i);
+            char c = char.Parse("w");
+            Console.WriteLine("Value of c: {0}", c);
+            Console.WriteLine();
+        }
+
+        static void ParseFromStringWithTryParse()
+        {
+            Console.WriteLine("=> Data type parsing with TryParse:");
+            if (bool.TryParse("True", out bool b)) 
+            {
+                Console.WriteLine("Value of b: {0}", b);
+            }
+            string value = "Hello";
+            if (double.TryParse(value, out double d))
+            {
+                Console.WriteLine("Value of d: {0}", d);        
+            }
+            else
+            {
+                Console.WriteLine("Failed to convert the input ({0}) to a double", value);
+            }
             Console.WriteLine();
         }
     }
